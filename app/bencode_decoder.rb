8a32f8968c27fe467bcf186238eab766307e633e
Yaquelqun <sandjiv.parassou@gmail.com>
2023-10-20 16:43:24 +0200

    syntax issue

--- app/bencode_decoder.rb
+++ app/bencode_decoder.rb
@@ -51,10 +51,10 @@ class BencodeDecoder
     input.match(/(i[-\d]*e)/)[1].length
   end
 
-    # returns the length of the first encoded element if it is a list
-    def encoded_list_length(input)
-      input.match(/(l.*e)/)[1].length
-    end
+  # returns the length of the first encoded element if it is a list
+  def encoded_list_length(input)
+    input.match(/(l.*e)/)[1].length
+  end
 
   # returns a decoded array if the first element of the input is a list
   def decode_list(input)
