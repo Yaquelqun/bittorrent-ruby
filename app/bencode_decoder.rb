47848c32183f1da8555a540845a96d352af99e4f
Yaquelqun <sandjiv.parassou@gmail.com>
2023-10-20 16:53:49 +0200

    add dictionary support

--- app/bencode_decoder.rb
+++ app/bencode_decoder.rb
@@ -23,11 +23,15 @@ class BencodeDecoder
       :integer
     when 'l'
       :list
+    when 'd'
+      :dictionary
     else
       puts "unsupported_format"
       exit(1)
     end
   end
+
+  ### STRING ###
 
   # returns a decoded string if the first element of the input is a string
   def decode_string(input)
@@ -41,6 +45,8 @@ class BencodeDecoder
     input.split(':')[0].length + 1 + length
   end
 
+  ### Integer ###
+
   # returns a decoded string if the first element of the input is an integer
   def decode_integer(input)
     input[1..-2].to_i
@@ -51,10 +57,7 @@ class BencodeDecoder
     input.match(/(i[-\d]*e)/)[1].length
   end
 
-  # returns the length of the first encoded element if it is a list
-  def encoded_list_length(input)
-    input.match(/(l.*e)/)[1].length
-  end
+  ### List ###
 
   # returns a decoded array if the first element of the input is a list
   def decode_list(input)
@@ -70,5 +73,27 @@ class BencodeDecoder
     end
 
     result
+  end
+
+  # returns the length of the first encoded element if it is a list
+  def encoded_list_length(input)
+    input.match(/(l.*e)/)[1].length
+  end
+
+  ### Dictionary ###
+
+  # returns a decoded array if the first element of the input is a dictionnary
+  def decode_dictionary(input)
+    array_version = decode_list(input)
+    dictionnary = []
+    # fun Ruby fact, you can turn an array of arrays that each have 2 elements into a hash by calling .to_h
+    # so decoding a dictionary is very basically decoding a list with extra steps
+    dictionnary << array_version.shift(2) until array_version.empty?
+    dictionnary.to_h
+  end
+
+  # returns the length of the first encoded element if it is a dictionnary
+  def encoded_dictionary_length(input)
+    input.match(/(d.*e)/)[1].length
   end
 end
